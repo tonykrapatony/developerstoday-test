90af995e0445cdcebc92780aba8e50d3507a9708
tonykrapatony <tonykrapatony@gmail.com>
2025-05-11 21:11:02 +0300

    added nav

--- src/app/recipes/[id]/page.tsx
+++ src/app/recipes/[id]/page.tsx
@@ -1,3 +1,4 @@
+import PageContainer from '@/components/PageContainer';
 import Image from 'next/image';
 import { notFound } from 'next/navigation';
 
@@ -34,26 +35,28 @@ const fetchRecipeDetails = async (id: string): Promise<RecipeDetail> => {
   return res.json();
 };
 
-export default async function RecipeDetailsPage({ params }: { params:Promise<{ id: string }> }) {
-  const  { id } = await params;
+export default async function RecipeDetailsPage({ params }: { params: Promise<{ id: string }> }) {
+  const { id } = await params;
   const recipe = await fetchRecipeDetails(id);
 
   return (
-    <div className="max-w-3xl mx-auto p-6">
-      <h1 className="text-3xl font-bold mb-4">{recipe.title}</h1>
-      <Image src={recipe.image} alt={recipe.title} className="w-full object-cover mb-4" width={500} height={500} />
-      <p className="text-gray-600 mb-2">⏱ Time: {recipe.readyInMinutes} min | 🍽 Servings: {recipe.servings}</p>
+    <PageContainer>
+      <div className="max-w-3xl mx-auto p-6">
+        <h1 className="text-3xl font-bold mb-4">{recipe.title}</h1>
+        <Image src={recipe.image} alt={recipe.title} className="w-full object-cover mb-4" width={500} height={500} />
+        <p className="text-gray-600 mb-2">⏱ Time: {recipe.readyInMinutes} min | 🍽 Servings: {recipe.servings}</p>
 
-      <div className="prose prose-sm prose-gray mb-6" dangerouslySetInnerHTML={{ __html: recipe.summary }} />
+        <div className="prose prose-sm prose-gray mb-6" dangerouslySetInnerHTML={{ __html: recipe.summary }} />
 
-      <h2 className="text-2xl font-semibold mb-3">Ingredients</h2>
-      <ul className="list-disc pl-5">
-        {recipe.extendedIngredients.map((ing) => (
-          <li key={ing.id}>
-            {ing.amount} {ing.unit} {ing.name}
-          </li>
-        ))}
-      </ul>
-    </div>
+        <h2 className="text-2xl font-semibold mb-3">Ingredients</h2>
+        <ul className="list-disc pl-5">
+          {recipe.extendedIngredients.map((ing) => (
+            <li key={ing.id}>
+              {ing.amount} {ing.unit} {ing.name}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </PageContainer>
   );
 }
